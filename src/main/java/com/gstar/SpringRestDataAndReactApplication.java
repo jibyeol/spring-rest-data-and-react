f4595e17c25532c822ce20c02e663519ffefdcf3
jibyeol <cjfrtt1234@naver.com>
2017-06-06 17:22:23 +0900

    프로젝트 생성
    프로젝트 생성

---
+++ src/main/java/com/gstar/SpringRestDataAndReactApplication.java
@@ -0,0 +1,12 @@
+package com.gstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDataAndReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDataAndReactApplication.class, args);
+	}
+}
